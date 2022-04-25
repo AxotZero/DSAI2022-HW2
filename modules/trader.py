576893f3cbeb8643544430f8e2f543601f22e18f
AxotZero <asdswegreh@gmail.com>
2022-04-25 13:45:12 +0800

    kfold

--- modules/trader.py
+++ modules/trader.py
@@ -31,14 +31,97 @@ class TraderMode(IntEnum):
 
 class Trader:
     def __init__(self, 
-                 epochs=400, lr=1e-2, model_hidden_size=16,
+                 kfold=5,
+                 epochs=400, 
+                 lr=1e-2, 
+                 model_hidden_size=16,
                  num_past=20):
 
         self.num_past = num_past
+        self.kfold = kfold
         self.epochs = epochs
         self.model_hidden_size = model_hidden_size
         self.lr = lr
+        self.models = []
+
+    def train_one_fold(self, datas, targets, fold_idx=0, num_fold=5):
+        print(f'=== train fold {fold_idx+1} ===')
+
+        # compute valid size
+        valid_ratio = 1 / num_fold if num_fold > 1 else 0.2
+        valid_size = round(valid_ratio * datas.shape[0])
+
+        # split train, valid
+        idx_full = np.arange(len(datas))
+        valid_idx = idx_full[fold_idx*valid_size: (fold_idx+1)*valid_size]
+        first_idx, last_idx = valid_idx[0], valid_idx[-1]
+
+        delete_idx = []
+        delete_idx.append(valid_idx)
+        if fold_idx != 0:
+            delete_idx.append(np.array(range(valid_idx[0]-self.num_past, valid_idx[0])))
+        if fold_idx != self.kfold-1:
+            delete_idx.append(np.array(range(valid_idx[-1], valid_idx[-1] + self.num_past)))
+        delete_idx = np.concatenate(delete_idx)
+            
+        train_idx = np.delete(idx_full, delete_idx)
+
+        train_x, train_y = datas[train_idx], targets[train_idx]
+        valid_x, valid_y = datas[valid_idx], targets[valid_idx]
+
+        train_x = torch.tensor(train_x).float()
+        train_y = torch.tensor(train_y).float()
+        valid_x = torch.tensor(valid_x).float()
+        valid_y = torch.tensor(valid_y).float()
         
+        # training setup
+        model = GruRatioRegressor(
+            num_feat=train_x.size()[-1],
+            num_out= 2,
+            hidden_size=self.model_hidden_size,
+        )
+
+        optimizer = torch.optim.Adam(
+            model.parameters(),
+            lr=self.lr
+        )
+        criterion = nn.MSELoss()
+
+        # core metric
+        best_val_loss = float("inf")
+        best_epoch = -1
+
+        # start training
+        pbar = tqdm(range(self.epochs), ncols=120)
+        for epoch in pbar:
+
+            # train
+            model.train()
+            pred, _ = model(train_x)
+            train_loss = criterion(pred, train_y)
+            train_loss.backward()
+            optimizer.step()
+            optimizer.zero_grad()
+
+            # valid
+            model.eval()
+            pred, _ = model(valid_x)
+            valid_loss = criterion(pred, valid_y)
+
+            pbar.set_description(f'epoch: {epoch}, train_mse: {train_loss.item():.6f}, valid_mse: {valid_loss.item():.6f}')
+            pbar.update()
+
+            # check whether get better result
+            if valid_loss.item() < best_val_loss:
+                best_val_loss = valid_loss.item()
+                best_epoch = epoch
+                torch.save(model.state_dict(), f"./data/model_data/model{fold_idx}.pth")
+        print(f'fold: {fold_idx+1}, best_val_loss: {best_val_loss:.6f} at epoch {best_epoch}')
+
+        return model
+
+
+
     def train(self, data):
         self.mode = TraderMode.TRAINING
         
@@ -62,90 +145,24 @@ class Trader:
         datas = np.array(datas)
         targets = np.array(targets)
 
-        # shuffle
-        idx_full = np.arange(len(datas))
-        np.random.seed(0)
-        np.random.shuffle(idx_full)
-        datas = datas[idx_full]
-        targets = targets[idx_full]
 
-        # train_valid split
-        valid_ratio = 0.2
-        valid_size = round(valid_ratio * datas.shape[0])
-        train_size = datas.shape[0] - valid_size
+        for i in range(self.kfold):
+            model = self.train_one_fold(datas, targets, fold_idx=i, num_fold=self.kfold)
+            self.models.append(model)
 
-        train_x, train_y = datas[:train_size], targets[:train_size]
-        valid_x, valid_y = datas[train_size:], targets[train_size:]
-
-
-        # convert to tensor
-        train_x = torch.tensor(train_x).float()
-        train_y = torch.tensor(train_y).float()
-        valid_x = torch.tensor(valid_x).float()
-        valid_y = torch.tensor(valid_y).float()
-
-        print(train_x.size())
-        print(train_y.size())
-        print(valid_x.size())
-        print(valid_y.size())
-
-        # training setup
-        self.model = GruRatioRegressor(
-            num_feat=4,
-            num_out= 2,
-            hidden_size=self.model_hidden_size,
-        )
-        optimizer = torch.optim.Adam(
-            self.model.parameters(),
-            lr=self.lr
-        )
-        criterion = nn.MSELoss()
-
-        # core metric
-        best_val_loss = float("inf")
-        best_epoch = -1
-
-        # start training
-        for epoch in range(self.epochs):
-
-            print(f'=== epoch: {epoch} ===')
-
-            # train
-            self.model.train()
-            pred, _ = self.model(train_x)
-            # bp()
-            loss = criterion(pred, train_y)
-            loss.backward()
-            optimizer.step()
-            optimizer.zero_grad()
-            print(f'Train epoch: {epoch}, mse: {loss.item() :.6f}')
-
-            # valid
-            self.model.eval()
-            pred, _ = self.model(valid_x)
-            loss = criterion(pred, valid_y)
-            print(f'Valid epoch: {epoch}, mse: {loss.item() :.6f}')
-            
-            # check whether get better result
-            if loss.item() < best_val_loss:
-                best_val_loss = loss.item()
-                best_epoch = epoch
-                torch.save(self.model.state_dict(), "model.pth")
-
-    
-        print('=== Stop Training ===')
-        print(f'Best val_loss: {best_val_loss:.6f} at epoch {best_epoch}')
     
     def predict_action(self, row):
         
         # first prediction
         if self.mode == TraderMode.TRAINING:
+            print('=== start run test ===')
+
             self.mode = TraderMode.TESTING
 
             # load model
-            self.model.load_state_dict(torch.load("model.pth"))
-            self.model.eval()
-
+            for i, model in enumerate(self.models):
+                model.load_state_dict(torch.load(f"./data/model_data/model{i}.pth"))
+                model.eval()
             self.cur_sum = 0
 
 
@@ -156,14 +173,17 @@ class Trader:
         _input = self.raw_data[-self.num_past:]
         
         # predict
+        preds = []
         with torch.no_grad():
             _input = torch.tensor(_input).unsqueeze(0).float()
-            pred, ht = self.model(_input, )
+            for model in self.models:
+                pred, _ = model(_input)
+                pred = pred[:, -1].detach().squeeze().numpy()
+                preds.append(pred)
+            pred = np.mean(np.array(preds), axis=1)
 
 
         # generate action
-        pred = pred[:, -1].detach().squeeze().numpy()
-        # action = BUY_IDX if pred[1] > pred[0] else SELL_IDX
         action = BUY_IDX if pred[1] > pred[0] else SELL_IDX
         print(action, end=' ')
         # print(pred)
